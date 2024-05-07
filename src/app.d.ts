c481dbc5c4d3a7fb2497b2fd80d9c4eacc4f9acb
OllyNicholass <OllyNicholass@users.noreply.github.com>
2024-05-07 20:32:04 +0100

    chore(*): update app declaration with supabase data

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,18 +1,22 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
+import { SupabaseClient, Session, User } from '@supabase/supabase-js'
 declare global {
 	const __DATE__: string;
 	const __RELOAD_SW__: boolean;
 	namespace App {
 		// interface Error {}
 		interface Locals {
+			supabase: SupabaseClient
+      safeGetSession(): Promise<{ session: Session | null; user: User | null }>
 			userid: string;
 			buildDate: string;
 			periodicUpdates: boolean;
 		}
-
-		// interface PageData {}
+		interface PageData {
+      session: Session | null
+    }
 		// interface PageState {}
 		// interface Platform {}
 	}
