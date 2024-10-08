2f7ec9860ad0959c3102b8e732c64d195fcafaa1
Josh Creek <8179928+jcreek@users.noreply.github.com>
2024-10-08 20:14:29 +0100

    feat(#32): Add separate flow for subscriptions

--- src/routes/products/[productId]/+server.ts
+++ src/routes/products/[productId]/+server.ts
@@ -4,5 +4,10 @@ import { stripe as stripeClient } from '$lib/utils/stripe';
 export const GET = async ({ params }) => {
 	const { productId } = params;
 	const product = await stripeClient.products.retrieve(productId);
+
+	const price = await stripeClient.prices.retrieve(product.default_price);
+
+	const isSubscription = price.type === 'recurring';
+	product.isSubscription = isSubscription;
 	return json(product);
 };
