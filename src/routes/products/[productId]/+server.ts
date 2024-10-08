e51aeb7c6320b8f01ed515827540590d7cdfb158
Josh Creek <8179928+jcreek@users.noreply.github.com>
2024-10-08 20:32:24 +0100

    feat(#32): Strongly type the price.type

--- src/routes/products/[productId]/+server.ts
+++ src/routes/products/[productId]/+server.ts
@@ -1,5 +1,6 @@
 import { json } from '@sveltejs/kit';
 import { stripe as stripeClient } from '$lib/utils/stripe';
+import Stripe from 'stripe';
 
 export const GET = async ({ params }) => {
 	const { productId } = params;
@@ -7,7 +8,7 @@ export const GET = async ({ params }) => {
 
 	const price = await stripeClient.prices.retrieve(product.default_price);
 
-	const isSubscription = price.type === 'recurring';
+	const isSubscription = (price.type as Stripe.Price.type) === 'recurring';
 	product.isSubscription = isSubscription;
 	return json(product);
 };
