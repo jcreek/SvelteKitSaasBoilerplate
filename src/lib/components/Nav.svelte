6ed1b255839e62e27c88f041798ee33565b8cc1b
OllyNicholass <OllyNicholass@users.noreply.github.com>
2024-10-22 22:19:19 +0100

    refactor(#35): conditionally display basket indicator

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -69,7 +69,9 @@
               d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
             />
           </svg>
-          <span class="badge badge-sm indicator-item">{localBasket.items.length}</span>
+          {#if localBasket.items.length > 0}
+            <span class="badge badge-sm indicator-item">{localBasket.items.length}</span>
+          {/if}
         </div>
       </a>
 
