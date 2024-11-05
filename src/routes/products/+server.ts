57a800ef2c8d5fc7425337debf66d99109466430
OllyNicholass <OllyNicholass@users.noreply.github.com>
2024-11-05 22:13:20 +0000

    refactor(#65): add types to products page params

--- src/routes/products/+server.ts
+++ src/routes/products/+server.ts
@@ -3,11 +3,11 @@ import { getActiveProductsWithPrices } from '$lib/utils/supabase/admin';
 import logger from '$lib/utils/logger/logger';
 
 export const GET = async ({ url }) => {
-	const limitParam = url.searchParams.get('limit');
-	const offsetParam = url.searchParams.get('offset');
+	const limitParam: string | null = url.searchParams.get('limit');
+	const offsetParam: string | null = url.searchParams.get('offset');
 	try {
-		const limit = limitParam ? parseInt(limitParam, 10) : 10;
-		const offset = offsetParam ? parseInt(offsetParam, 10) : 0;
+		const limit = limitParam ? Number.parseInt(limitParam, 10) : 10;
+		const offset = offsetParam ? Number.parseInt(offsetParam, 10) : 0;
 
 		const { products, count } = await getActiveProductsWithPrices(limit, offset);
 
