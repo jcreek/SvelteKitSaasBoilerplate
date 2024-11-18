180325449cb261af416a609744224e6c37347a69
OllyNicholass <OllyNicholass@users.noreply.github.com>
2024-11-18 23:07:51 +0000

    refactor(#35): hide basket icon from SR

--- src/lib/components/navigation/Nav.svelte
+++ src/lib/components/navigation/Nav.svelte
@@ -35,6 +35,7 @@
 							stroke="currentColor"
 							viewBox="0 0 24 24"
 							xmlns="http://www.w3.org/2000/svg"
+							aria-hidden="true"
 						>
 							<path
 								stroke-linecap="round"
